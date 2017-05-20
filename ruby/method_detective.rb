051f890b1f8815d652dbb8d2a37f6a0b33115448
Scott Fulton <sfulton70@yahoo.com>
2017-05-19 21:35:30 -0700

    Finish Challenge 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+# "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".insert(1, "o")
-"zom".insert(2, "o")
+# "zom".insert(1, "o")
+# "zom".insert(2, "o")
 # => “zoom”
 
-"enhance".center(15)
+# "enhance".center(15)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".insert(-1, " suspects")
+# "the usual".insert(-1, " suspects")
 #=> "the usual suspects"
 
-" suspects".prepend("the usual")
+# " suspects".prepend("the usual")
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
+# "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".slice(1,38)
+# "The mystery of the missing first letter".slice(1,38)
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
+# "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-"z".each_byte {|b| p b}
+# "z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) It is the ASCII value of Z.
 
-
-"How many times does the letter 'a' appear in this string?".count "a"
+# "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
