16e4635e916b918eb69a9665e62c9718336914ba
Scott Fulton <sfulton70@yahoo.com>
2017-05-23 12:11:22 -0700

    Finish Challenge 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Canis lupus familiaris
+
+# CHARACTERISTICS ----------------
+# Weight: 9 pounds
+# Likes children: Yes
+# Length: 19 inches
+# Speed when running: 5 MPH
+# Color: varies
+# Likes being taken on walks: Yes
+
+# BEHAVIOR -----------------------
+# Barks at Mailman
+# Bites
+# Poops where it shouldn't
+# Eats bacon
