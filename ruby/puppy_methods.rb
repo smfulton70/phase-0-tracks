b2039618eacc92887b9ec0ee9c16b9d88d024e53
Scott Fulton <sfulton70@yahoo.com>
2017-05-23 21:04:12 -0700

    Created DBC Student class and instantiated 50 students of this class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -21,6 +21,11 @@ class Puppy
   end
 end
 
+
+
+
+
+
 our_puppy = Puppy.new
 
 our_puppy.fetch("squeaky toy")
@@ -28,3 +33,28 @@ our_puppy.speak(5)
 our_puppy.roll_over
 puts our_puppy.dog_years(10)
 our_puppy.bark_at_mailman(80)
+
+class DBC_student
+	def initialize
+		puts "Creating a new DBC student..."
+	end
+
+	def study(hours)
+		puts "I study #{hours} hours a week."
+	end
+
+	def drink_coffee(cups_a_day)
+		puts "I drink #{cups_a_day} cups of Sumatra coffee a day!"
+	end
+end
+
+array_of_DBC_students = Array.new
+50.times do |x| 
+	array_of_DBC_students << DBC_student.new
+end
+puts array_of_DBC_students.length
+
+array_of_DBC_students.each do |student|
+	student.study(25)
+	student.drink_coffee(3)
+end 
