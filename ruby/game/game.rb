cba2f794e1264cca06828e1711e4188e376a4b79
Scott Fulton <sfulton70@yahoo.com>
2017-05-28 21:58:05 -0700

    Make the game so that the user is not punished for repeating a guess

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,10 +1,11 @@
 
 
 class Game 
-	attr_accessor :word_to_guess_array, :guess, :word
+	attr_reader :word_to_guess
+	attr_accessor :word_to_guess_array, :guess, :word_so_far
 	def initialize
 		puts "Starting a new game!"
-		@word_to_guess = "DaisysDonuts"
+		@word_to_guess = "cat"
 		@word_to_guess_array = @word_to_guess.split("")
 		@word_to_guess_array.map! do |value|
 			value.prepend("-")
@@ -13,7 +14,7 @@ class Game
 
 	def display_word_status(guess)
 		@guess = guess
-		@word = ""
+		@word_so_far = ""
 		@word_to_guess_array.map! do |value|
 			if value[1] == @guess
 				value.slice!(1)
@@ -21,37 +22,36 @@ class Game
 			end
 		end
 		@word_to_guess_array.each do |value|
-			@word += "#{value[0]}"
+			@word_so_far += "#{value[0]}"
 		end	
-		return @word
+		return @word_so_far
 	end
 end
 
-game1 = Game.new
+game = Game.new
 
 puts "I am thinking of a word pertaining to Ruby and you have to guess what it is by inputting a letter in each guess."
 puts "This is what we currently have: "
 puts ""
-game1.word_to_guess_array.each do |value|
+game.word_to_guess_array.each do |value|
 	print value[0]
 end
 puts ""
 puts ""
-puts "What is your letter guess?"
-game1.guess = gets.chomp
-puts ""
-puts "And here is what we have now:"
-puts ""
-puts game1.display_word_status(game1.guess)
+counter=0
 loop do
-	puts "Please enter another letter:"
-	game1.guess = gets.chomp
+	puts "Please enter a letter:"
+	game.guess = gets.chomp
 	puts ""
 	puts "And here is what we have now:"
 	puts ""
-	puts game1.display_word_status(game1.guess)
+	puts game.display_word_status(game1.guess)
 	puts ""
-	if !game1.word.match('-')
+	counter+=1
+	if (counter == 2*game.word_to_guess.length && game.word_so_far.match('-'))
+		puts "You are an absolute disaster, an utter failure. You could not guess the word in twice the number of guesses as there were	letters in the puzzle. Sad! The word was #{game1.word_to_guess}"
+		break
+	elsif !game.word_so_far.match('-')
 		puts "Congratulations! You have solved the puzzle! Vanna, you are amazing as always!"
 		break
 	end
