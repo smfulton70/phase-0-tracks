1268b2d2d4433ad24aabe4abc471330d0e8ba525
Scott Fulton <sfulton70@yahoo.com>
2017-05-31 12:31:03 -0700

    Create horse object comprised of horseName and horseColor

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,3 +4,15 @@ horseName = ["abe","fido","ed","charley"];
 horseColor.push("white");
 horseName.push("rhinestone");
 
+function horseCreator(horseName,horseColor){
+	var horse = {}
+	for (i=0; i<horseName.length; i++){
+		horse[horseName[i]] = horseColor[i];
+	}
+return horse;
+
+}
+
+console.log(horseCreator(horseName,horseColor));
+
+
