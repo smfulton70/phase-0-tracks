6771d139d0aca4c33a67d5d7701db13e48d258d8
Scott Fulton <sfulton70@yahoo.com>
2017-06-02 21:20:21 -0700

    Merge in the final pseudocode into the file

--- js/algos.js
+++ js/algos.js
@@ -1,9 +1,9 @@
 // RELEASE 0
 
-/*Create a function that takes an array of words as an argument. We set the first word in that array to be the longest word so far.
+/* Create a function that takes an array of words as an argument. We set the first word in that array to be the longest word so far.
 We then iterate through the array of words and each time we encounter a word that is longer than the previous word, we establish this
 word to be the longest word so far. By the time we have iterated through the entire array of words, we return the longest word so far.
-If there are two or more words that are the longest words, the first word encountered gets returned.*/
+If there are two or more words that are the longest words, the first word encountered gets returned. */
 
 function longestPhrase(array){
 	var array_of_words = array;
@@ -17,6 +17,14 @@ function longestPhrase(array){
 }
 
 // RELEASE 1
+
+/* Create a function that takes two objects as arguments. We create an array that will be used to accumulate elements, each of which is 
+a key item pair that is made into a simple string with a space separating the key from the value. Using a for..in loop, we iterate
+through the first object and for each key value pair, we create the string and push it into the array we have created. We then iterate
+through all of the key value pairs in the second object and create them into simple strings as we did for the first object, and for each 
+key value pair string, we test it to see if that string is found in the array containing the first object's key value strings. All we have
+to do is find ONE match and then we return a boolean of true and break from the loop and return true. Otherwise, we continue to iterate
+and once we are through the loop and find no matches, then we return false. */
 
 function findKeyValueMatch(object1,object2){
 	var object1;
@@ -38,6 +46,13 @@ function findKeyValueMatch(object1,object2){
 
 // RELEASE 2
 
+/* Create a function that takes an integer as an argument, and that integer is the number of strings that the function will create.
+However many strings it will create in one call of the method, each string will be of a random length between 1 and 10 characters. There
+will be 62 characters for the strings to be comprised of (All letters in the alphabet, both capital and lower case, and the digits 0-9).
+We have an outer loop that is iterated the same number of times as number of strings there should be, and the inner loop iterates the
+same number of times as characters there should be in a string. Each iteration of the inner loop generates a string that is fed into an 
+array of strings. The whole method returns the array of strings. */
+
 function randomTestData(integer){
 	var integer;
 	var array_of_strings = [];
@@ -49,14 +64,7 @@ function randomTestData(integer){
 			array_of_strings[i] += possibleCharacters.charAt(Math.floor((Math.random() * 62) + 1))
 		}
 	}
-	//for (i=0;i<integer;i++){
-		//console.log(array_of_strings[i]);
-	//}
-
 	return array_of_strings;
-
-	//return "Here are the strings: " + array_of_strings[0] + " " + array_of_strings[1] + " " + array_of_strings[2];
-	//return randomLengthOfStrings;
 }
 
 // RELEASE 0 DRIVER CODE
