d13881bd2fdf604ec7cd5521cb033646951907e6
Scott Fulton <sfulton70@yahoo.com>
2017-06-11 14:57:25 -0700

    Create a user interface that allows user to enter values to crete records in the rollercoaster table

--- databases/kitten_maker/rollercoaster_factory.rb
+++ databases/kitten_maker/rollercoaster_factory.rb
@@ -14,66 +14,65 @@ db.results_as_hash = true
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS rollercoasters(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
+    coaster_name VARCHAR(255),
     park VARCHAR(255),
     location VARCHAR(255),
-    year_opened DATE,
-    number_of_loops INTEGER,
-    height_in_feet INTEGER,
+    year_opened INT,
+    number_of_loops INT,
+    height_in_feet INT,
     floorless BOOLEAN,
     wooden BOOLEAN
   )
 SQL
 
-# create a kittens table (if it's not there already)
+# create a rollercoaster table (if it's not there already)
 db.execute(create_table_cmd)
 
-# add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
-
-# add LOOOOTS of kittens!
-# so. many. kittens. 
-#KittenExplosion
-def create_rollercoaster(db, name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden)
-  db.execute("INSERT INTO rollercoasters (name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden) VALUES (?, ?, ?, ?, ?, ?, ?)", [name, park, location, number_of_loops, height_in_feet, floorless?, wooden?])
+# insert rollercoaster records into the rollercoaster table
+def create_rollercoaster(db, coaster_name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden)
+  db.execute("INSERT INTO rollercoasters (coaster_name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden) VALUES (?,?,?,?,?,?,?,?)", [coaster_name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden])
 end
 
-# rollercoaster_array = []
-# 3.times do |i|
-#   puts "Please enter a name of a rollercoaster:"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Please enter the park where this rollercoaster is located:"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Please enter city and state where this park is located (in the format of CITY, ST):"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Please enter the year this rollercoaster opened:"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Please enter the number of loops this rollercoaster has:"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Please enter the height in feet of this rollercoaster:"
-#   rollercoaster_array[i].push(gets.chomp)
-#   puts "Do your feet touch the floor in this rollercoaster? (Y or N):"
-#   case gets.chomp
-#   		when "N","n"
-#   			rollercoaster_array[i].push(true)
-#   		when "Y","y"
-#   			rollercoaster_array[i].push(false)	
-#   end
-#   puts "Is this rollercoaster wooden? (Y or N):"
-#   case gets.chomp
-#   		when "Y","y"
-#   			rollercoaster_array[i].push(true)
-#   		when "N","n"
-#   			rollercoaster_array[i].push(false)	
-#   end
-#   create_rollercoaster(db, rollercoaster_array[i])
-# end
+10.times do |i|
+  puts "Please enter a name of a rollercoaster:"
+  coaster_name = gets.chomp
+  puts "Please enter the park where this rollercoaster is located:"
+  park = gets.chomp
+  puts "Please enter city and state where this park is located (in the format of CITY, ST):"
+  location = gets.chomp
+  puts "Please enter the year this rollercoaster opened:"
+  year_opened =gets.chomp.to_i
+  puts "Please enter the number of loops this rollercoaster has:"
+  number_of_loops = gets.chomp.to_i
+  puts "Please enter the height in feet of this rollercoaster:"
+  height_in_feet = gets.chomp.to_i
+  puts "Do your feet touch the floor in this rollercoaster? (Y or N):"
+  case gets.chomp
+  when "N","n","No","no","NO"
+  	floorless = "true"
+  when "Y","y","Yes","yes","YES"
+ 	floorless = "false"	
+  end
+  puts "Is this rollercoaster wooden? (Y or N):"
+  case gets.chomp
+  when "Y","y","Yes","yes","YES"
+  		wooden = "true"
+  when "N","n","No","no","NO"
+  		wooden = "false"	
+  end
+  if i !=3
+  	puts "---------"
+    puts "New Rollercoaster:"
+    puts
+  end
+  create_rollercoaster(db, coaster_name, park, location, year_opened, number_of_loops, height_in_feet, floorless, wooden)
+end
 
 # explore ORM by retrieving data
-# rollercoasters = db.execute("SELECT * FROM rollercoasters")
-# rollercoasters.each do |rollercoaster|
-# puts "#{rollercoaster['name']} is located in the park called #{rollercoaster['park']} in #{rollercoaster['location']}. It opened in #{rollercoaster['date']}
-# and it has #{rollercoaster['number of loops']} loops. It is #{rollercoaster['height_in_feet']} feet tall, and the floorless status is #{rollercoaster['floorless?']}
-# and the wooden status is #{rollercoaster['wooden?']}"
-# end
+rollercoasters = db.execute("SELECT * FROM rollercoasters")
+
+rollercoasters.each do |rollercoaster|
+	puts "#{rollercoaster['coaster_name']} is located in the park called #{rollercoaster['park']} in #{rollercoaster['location']}. It opened in #{rollercoaster['year_opened']} and it has #{rollercoaster['number_of_loops']} loops. It is #{rollercoaster['height_in_feet']} feet tall, and the floorless status is #{rollercoaster['floorless']} and the wooden status is #{rollercoaster['wooden']}."
+	puts
+end
 
